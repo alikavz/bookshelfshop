8b9dcee78842c1794c38ce24cb4310705d38f096
alikavz <garry_edmilson@yahoo.com>
2024-07-22 13:00:47 +0430

    comment section

--- bookha/models.py
+++ bookha/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
@@ -14,4 +15,15 @@ class Book(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', args=[self.id])
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    date = models.DateField(auto_now_add=True)
+    Which_book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user}:{self.text}'
+
 
