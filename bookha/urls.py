6594ab7f058bcc47cb5a2482ebaae48464b27d58
alikavz <garry_edmilson@yahoo.com>
2024-07-22 19:59:48 +0430

    new deteailview with cm

--- bookha/urls.py
+++ bookha/urls.py
@@ -1,10 +1,10 @@
-from .views import Booklis, Bookdetail, Creating, Updateview, Deleting
+from .views import Booklis, bookdetail, Creating, Updateview, Deleting
 from django.urls import path, include
 
 
 urlpatterns = [
     path('', Booklis.as_view(), name='booklis'),
-    path('<int:pk>/', Bookdetail.as_view(), name='detail'),
+    path('<int:pk>/', bookdetail, name='detail'),
     path('create/', Creating.as_view(), name='create'),
     path('<int:pk>/update/', Updateview.as_view(), name='up_to_date'),
     path('<int:pk>/delete/', Deleting.as_view(), name='deleting'),
--- bookha/views.py
+++ bookha/views.py
@@ -1,6 +1,7 @@
 from django.urls import reverse_lazy
 from django.views import generic
-from .models import Book
+from .models import Book, Comment
+from django.shortcuts import get_object_or_404, render
 
 
 class Booklis(generic.ListView):
@@ -10,9 +11,14 @@ class Booklis(generic.ListView):
     context_object_name = 'bookss'
 
 
-class Bookdetail(generic.DetailView):
-    model = Book
-    template_name = 'bookha/bookdetail.html'  #html akhar faramoosh nashavad
+# class Bookdetail(generic.DetailView):
+#     model = Book
+#     template_name = 'bookha/bookdetail.html'  #html akhar faramoosh nashavad
+
+def bookdetail(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    comments = book.comments.all()
+    return render(request, 'bookha/bookdetail.html', {'book': book, 'comments': comments})
 
 
 class Creating(generic.CreateView):
